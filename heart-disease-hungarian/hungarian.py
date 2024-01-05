75f06ad2afd0e4c9691fab55dc0fca28c4c01e07
tdindarisky <101049137+tdindarisky@users.noreply.github.com>
2024-01-05 16:44:50 +0700

    Add files via upload

--- heart-disease-hungarian/hungarian.py
+++ heart-disease-hungarian/hungarian.py
@@ -126,7 +126,7 @@ y = dfClean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
-model = pickle.load(open("model/knn_model.pkl", "rb"))
+model = pickle.load(open("model/xgb_model.pkl", "rb"))
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
